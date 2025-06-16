32a7b79eb7d3c94cd7e0fd28f1c2f637a4b2bc14
teja137 <ajetdream@yahoo.com>
2025-06-17 02:13:52 +0530

    fixing galaxy

--- src/app/galaxy/galaxy.component.ts
+++ src/app/galaxy/galaxy.component.ts
@@ -35,6 +35,7 @@ export class GalaxyComponent implements AfterViewInit, OnDestroy {
     this.initScene();
     this.animate();
     this.hideInfo();
+    this.toggleFullscreen();
   }
 
   ngOnDestroy(): void {
